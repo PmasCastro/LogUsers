b4ce9d3319a9102c0e6fe5b54e5f64e5cf62dd6b
PmasCastro <pmas.castro@gmail.com>
2025-04-23 01:19:31 +0100

    minor change

--- controller.py
+++ controller.py
@@ -2,9 +2,7 @@ from app import *
 from userdb import User
 
 def create_new_user():
-    from app import username_entry, password_entry
     
-
     username = username_entry.get()
     password = password_entry.get()
 
