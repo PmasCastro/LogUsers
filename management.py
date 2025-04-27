bd2117d9be97f3420083ce161c9b443459ddb237
PmasCastro <pmas.castro@gmail.com>
2025-04-27 18:50:33 +0100

    added function to change password

--- management.py
+++ management.py
@@ -1,4 +1,5 @@
 import sqlite3
+import bcrypt
 
 DB_NAME = "users.db"
 
@@ -12,55 +13,69 @@ class UserManagement:
             cursor = conn.cursor()
 
             # Check if the old_username exists in the database
-
             cursor.execute(
                 "SELECT username FROM users WHERE username=?", (old_username,))
             result = cursor.fetchone()
 
             # If the old_username does not exist, print a message and return
-
             if result is None:
                 print(f"User '{old_username}' does not exist.")
                 return
+            
+            # If the old_username exists, check if the new_username already exists
             else:
                 cursor.execute(
                     "SELECT username FROM users WHERE username=?", (new_username,))
-
                 result = cursor.fetchone()
+            
+            # If the new_username already exists, print a message and return
                 if result is not None:
                     print(f"User '{new_username}' already exists, please choose a different username.")
                     return
+                if " " in new_username:
+                    print("Username cannot contain spaces.")
+                    return
+                
+            # If the new_username does not exist, update the username    
                 else:
                     cursor.execute(
                         "UPDATE users SET username=? WHERE username=?", (new_username, old_username))
-
                     conn.commit()
                     print(f"User '{old_username}' changed to '{new_username}' successfully.")
-            
-        
-        
-            
         finally:
             conn.close()
+    
+    def change_password(self, username, new_password):
 
-        
-           
+        try:
+            conn = sqlite3.connect(DB_NAME)
+            cursor = conn.cursor()
 
+            # Check if the username exists in the database
+            cursor.execute(
+                "SELECT username FROM users WHERE username=?", (username,))
+            result = cursor.fetchone()
+
+            # If the username does not exist, print a message and return
+            if result is None:
+                print(f"User '{username}' does not exist.")
+                return
             
-                
-
-
-      
-
-
-
-
-
-
-
-
-    def change_password():
-        pass
+            ## If the username exists, check if the new_password is empty
+            else:
+                # If the new_password is empty, print a message and return
+                if " " in new_password:
+                    print("Password cannot contain spaces.")
+                    return
+                hashed = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+                # If the username exists, update the password
+                cursor.execute(
+                    "UPDATE users SET password=? WHERE username=?", (hashed, username))
+                conn.commit()
+                print(f"Password for user '{username}' changed successfully.")
+        finally:
+            conn.close()
+        
 
     def change_email():
         pass
@@ -73,7 +88,7 @@ class UserManagement:
 
 
 manager = UserManagement()
-manager.change_username("Admin2", "Admin")
+manager.change_password("Admin", "1234")
         
 
  
